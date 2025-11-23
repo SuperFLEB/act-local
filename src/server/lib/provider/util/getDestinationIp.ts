2eb67c356d27f87e8533e6fc90ff75954c240718
FLEB <voterudy@voterudy.org>
2025-11-22 23:59:55 -0500

    Handle "*" IPs in lsof provider

--- src/server/lib/provider/util/getDestinationIp.ts
+++ src/server/lib/provider/util/getDestinationIp.ts
@@ -1,5 +1,6 @@
 export default function getDestinationIp(ip: string) {
 	if (ip === "[::]") return "[::1]";
 	if (ip === "0.0.0.0") return "127.0.0.1";
+	if (ip === "*") return "127.0.0.1";
 	return ip;
 }
